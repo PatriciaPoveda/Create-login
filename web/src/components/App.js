517c361d77028c9f89dc05ce0a082deb95c19791
PatriciaPoveda <patriciapovedaboto@gmail.com>
2021-03-24 15:47:21 +0100

    finish login

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,47 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./Login";
 import api from "../services/api";
+import Header from "./Header";
+import Intranet from "./Intranet";
 
 const App = () => {
+  const [userData, setUserData] = useState("");
+  const [userId, setUserId] = useState("");
+  const [loginError, setLoginError] = useState("");
+
   const handlesignIn = (data) => {
     api.sendLogin(data).then((data) => {
       console.log(data);
-      //   if (data.error) {
-      //     // guardo el error en el estado para que se pinte
-      //     setLoginError(data.message);
-      //   } else {
-      //     // limpio el error
-      //     setLoginError("");
-      //     // guardo el usuario en el estado y en localStorage
-      //     setUserId(data.userId);
-      //     localStorage.set("user", data);
-      //   }
-      // });
+      if (data.error) {
+        // guardo el error en el estado para que se pinte
+        setLoginError(data.message);
+      } else {
+        // limpio el error
+        setLoginError("");
+        setUserData(data.userName);
+        setUserId(data.userId);
+      }
     });
   };
   const renderLogin = () => {
     return (
       <>
-        <Login handlesignIn={handlesignIn}></Login>
+        <Login handlesignIn={handlesignIn} loginError={loginError}></Login>
       </>
     );
   };
-  return renderLogin();
+  const renderIntranet = () => {
+    return (
+      <>
+        <Header userData={userData}></Header>
+        <Intranet></Intranet>
+      </>
+    );
+  };
+  return userId === "" ? renderLogin() : renderIntranet();
 };
 
 export default App;
-
-// import React, { useState } from "react";
-// import Header from "./Header";
-// import Intranet from "./Intranet";
-// const [userData, setUserData] = useState("");
-// setUserData(data.userName);
-// const renderIntranet = () => {
-//   return (
-//     <>
-//       <Header userData={userData}></Header>
-//       <Intranet></Intranet>
-//     </>
-//   );
-// };
-// return userData === "" ? renderLogin() : renderIntranet();
