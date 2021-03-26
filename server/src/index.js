fe7a49f6badcc83582cb07428ffe337a63af27dd
PatriciaPoveda <patriciapovedaboto@gmail.com>
2021-03-26 16:27:41 +0100

    add ejs

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 const cors = require("cors");
 const express = require("express");
 const apiUsers = require("./api/users");
+const films = require("../db/films-data.json");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.set("view engine", "ejs");
 
 const serverPort = 3000;
 app.listen(serverPort, () => {
@@ -14,3 +17,22 @@ app.listen(serverPort, () => {
 app.use(express.static("./public"));
 
 app.post("/api/login", apiUsers.login);
+
+app.get("/intranet/:filmId.html", (req, res) => {
+  // get film data
+  const filmData = films.find((film) => film.id === req.params.filmId);
+  console.log("film data", filmData);
+
+  // response with rendered template
+  if (filmData) {
+    // ensure data
+    filmData.title = filmData.title || "";
+    filmData.year = filmData.year || "";
+    filmData.director = filmData.director || "";
+    filmData.country = filmData.country || "";
+    filmData.awards = filmData.awards || [];
+    res.render("pages/film", filmData);
+  } else {
+    res.render("pages/film-not-found");
+  }
+});
